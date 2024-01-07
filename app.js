83bebaeb9add4bcb091e3e996ffdfdcd8f92238f
Mechanic Josh <joshuadunbar@gmail.com>
2024-01-06 21:29:18 -0500

    completed assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+
+let myName = 'Josh Dunbar';
+let capsName = myName.toUpperCase();
+console.log('Name: ' + capsName); 
+
+console.log('Career: Classic car restoration mechanic.');
+console.log("Description: I enjoy building and creating, anything using tools peaks my interest. I'm finding the logic and diagnostic approach to programming works with the way my mind thinks.");
+
+console.log("My Interests:");
+console.log('-Vintage vehicles');
+console.log('-Carpentry');
+console.log('-Bike riding');
+console.log('-Camping');
+
+console.log("My previous experience:");
+
+function displayPosition(companyName,jobTitle,description){
+    console.log(companyName, jobTitle, description);
+} 
+displayPosition('Patient First: ', 'Service Repairman - ', 'Install, maintain, and repair equipment used in the differing centers throughout Maryland.');
+displayPosition('Al-Hemp: ', 'Property Manager - ', 'Keep up tennant relations, handle rental payments, ensure necessary repairs to the property are made.');
+displayPosition('Sweet Frog: ', 'Kitchen staff - ', 'prep all fresh fruits and blend yogurt dessert mixtures' );
+
+console.log('My Skills:');
+
+function displaySkill(skill,isCool){
+    if(isCool == true){
+        console.log('BAM: ' + skill);
+    } else {
+        console.log(skill);
+    }
+}
+
+displaySkill('Welding', true);
+displaySkill('Built Tiny House', true);
+displaySkill('tells good jokes', false);
+displaySkill('best uncle', false);
+displaySkill('Build 1930s hot rods', true);
